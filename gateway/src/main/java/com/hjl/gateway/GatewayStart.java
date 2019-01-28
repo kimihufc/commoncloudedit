630bbfc2379a4fc528eeff73ca499f20f54b7318
hujianglai <hujianglai@yunzhangfang.com>
2019-01-28 19:26:32 +0800

    添加网关gateway

---
+++ gateway/src/main/java/com/hjl/gateway/GatewayStart.java
@@ -0,0 +1,17 @@
+package com.hjl.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: HJL
+ * @create: 2019-01-28 16:14
+ */
+@SpringBootApplication
+public class GatewayStart {
+
+    public static void main(String [] args){
+        SpringApplication.run(GatewayStart.class,args);
+    }
+
+}
